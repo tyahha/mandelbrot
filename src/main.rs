44a76f24cd586baa078ef2c15f05d59626250ce8
toshinori.sato <tyahhahha@gmail.com>
2023-05-12 23:49:41 +0900

    update main function

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,31 @@ use num::Complex;
 use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.30,0.35 -1,0.20", args[0]);
+
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+                    .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+                        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+                        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 }
 
 /// 大きさが`bounds`で指定されたバッファ`pixels`を`filename`で指定されたファイルに書き出す
